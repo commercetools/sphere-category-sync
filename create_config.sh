993f30ccb9a014b5636bdda5a71e527ab856da0e
Hajo Eichler <hajo.eichler@commercetools.de>
2014-08-04 21:15:10 +0200

    Add basic files.

---
+++ create_config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat > "config.js" << EOF
+/* SPHERE.IO credentials */
+exports.config = {
+  client_id: "${SPHERE_CLIENT_ID}",
+  client_secret: "${SPHERE_CLIENT_SECRET}",
+  project_key: "${SPHERE_PROJECT_KEY}"
+}
+
+cat > "${HOME}/.sphere-project-credentials" << EOF
+${SPHERE_PROJECT_KEY}:${SPHERE_CLIENT_ID}:${SPHERE_CLIENT_SECRET}
+EOF
